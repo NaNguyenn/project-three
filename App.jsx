fca1d980ff7d20587d71bf9ef0e9bd46574deec2
NguyennNguyenn <yu19151998@gmail.com>
2023-06-05 23:52:38 +0700

    Move App for building apk

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { AuthProvider } from './hooks/useAuth.js'
-import TabNavigator from './navigators/TabNavigator.jsx'
-import { VocabularyProvider } from './hooks/useVocabulary.js'
-import { UserScoreProvider } from './hooks/useUserScore.js'
+import { AuthProvider } from './src/hooks/useAuth.js'
+import TabNavigator from './src/navigators/TabNavigator.jsx'
+import { VocabularyProvider } from './src/hooks/useVocabulary.js'
+import { UserScoreProvider } from './src/hooks/useUserScore.js'
 
 export default function App() {
   return (
