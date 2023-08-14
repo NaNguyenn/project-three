74e6c2434f9a87eefe95cb766d2105601d16bd73
Vitj <annguyen.nguyen.gtsvietnam@gmail.com>
2023-08-14 17:04:56 +0700

    building levels selection in admin

--- src/screens/admin/index.jsx
+++ src/screens/admin/index.jsx
@@ -6,28 +6,50 @@ import SelectDropdown from "react-native-select-dropdown";
 
 const AdminScreen = () => {
   const vocabulary = useVocabulary();
-  const uniqueLevels = [...new Set(vocabulary.map((word) => word.level))];
-  const uniqueCategories = [
-    ...new Set(vocabulary.map((word) => word.category)),
-  ];
+  console.log(vocabulary[0]);
+
+  //Extract unique levels from vocabulary and assign a title
+  const uniqueLevels = [...new Set(vocabulary.map((word) => word.level))].map(
+    (level) => {
+      let property;
+      switch (level) {
+        case 1:
+          property = "Beginner";
+          break;
+        case 2:
+          property = "Intermediate";
+          break;
+        case 3:
+          property = "Advanced";
+          break;
+        case 4:
+          property = "IELTS";
+          break;
+        default:
+          property = "";
+      }
+      return { level, property };
+    }
+  );
 
   return (
     <SafeAreaView>
       <Text>Thêm từ vựng</Text>
       <SelectDropdown
         data={uniqueLevels}
+        defaultButtonText="Chọn cấp độ"
         onSelect={(selectedItem, index) => {
           console.log(selectedItem, index);
         }}
         buttonTextAfterSelection={(selectedItem, index) => {
           // text represented after item is selected
           // if data array is an array of objects then return selectedItem.property to render after item is selected
-          return selectedItem;
+          return selectedItem.property;
         }}
         rowTextForSelection={(item, index) => {
           // text represented for each item in dropdown
           // if data array is an array of objects then return item.property to represent item in dropdown
-          return item;
+          return item.property;
         }}
       />
     </SafeAreaView>
